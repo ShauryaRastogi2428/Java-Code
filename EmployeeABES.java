c3073e5b682c444e003cec47bf152a4f40b450d8
ShauryaRastogi2428 <shaurya.2428cseaiml2@kiet.edu>
2025-08-26 21:45:18 +0530

    Update EmployeeABES.java

--- EmployeeABES.java
+++ EmployeeABES.java
@@ -4,7 +4,7 @@ import KIET.*;
 public class EmployeeABES extends EmployeeKIET{
         public static void main(String[] args) {
         EmployeeABES ea = new EmployeeABES();
-        ea.Emp_Name = "Ronak Goel ABES";
+        ea.Emp_Name = "Shyam ABES";
         ea.Emp_ID = 301;
         // ea.Org = "ABES";
         // ea.Salary = 30000;
